eafe6efec8fb9f3a5965c2cfd6bcc27bbb9f6759
snowman <istming525@naver.com>
2021-10-02 16:43:30 +0900

    유저관리완성

--- src/userManagement.js
+++ src/userManagement.js
@@ -9,7 +9,10 @@ const getLoggedUser = () => localStorage.getItem('login-user');
 function addLogoutLayout(){
     const logoutButton = document.createElement('button');
     logoutButton.textContent = '로그아웃';
+    logoutButton.id = 'logout-button';
+    logoutButton.addEventListener('click',logout);
     userSection.appendChild(logoutButton);
+   
 }
 
 function isloggedIn() {
@@ -21,17 +24,22 @@ function isloggedIn() {
 }
 
 function repaintLoginAttributes(msg){
+    const signInput = document.getElementById('username');
+    signInput.textContent = '';
+    const sign = document.getElementById('blank');
+    
     userSection.removeChild(signInput);
     userSection.removeChild(sign);
     const welcomeSpanElement = document.createElement('span');
     welcomeSpanElement.textContent = msg;
+    welcomeSpanElement.id = 'welcome-msg';
     userSection.appendChild(welcomeSpanElement);
     addLogoutLayout();
 
 }
 
 function login() {
-    const username = signInput.value;
+    const username = document.getElementById('username').value;
     if(username.trim().length === 0)
         return window.alert('이름을 입력해주세요');
     const welcomeMessage = `${username}님 환영합니다.`;
@@ -40,6 +48,25 @@ function login() {
     window.localStorage.setItem('login-user', username);
 }
 
+function logout(){
+    const welcomeMsg = document.getElementById('welcome-msg');
+    const logoutBtn = document.getElementById('logout-button');
+    userSection.removeChild(welcomeMsg);
+    userSection.removeChild(logoutBtn);
+
+    const idInput = document.createElement('input');
+    idInput.id = 'username';
+    userSection.appendChild(idInput);
+
+    localStorage.removeItem('login-user');
+    const loginBtn = document.createElement('button');
+    loginBtn.id = 'blank';
+    loginBtn.textContent = 'Sign';
+    loginBtn.addEventListener('click',login);
+    userSection.appendChild(loginBtn);
+
+   
+}
 
 function onClickMessage(){
     window.alert(signInput.value);
@@ -61,4 +88,3 @@ function initialize(){
 }
 //함수가 실행되는 구역
 initialize();
-,
